cb751484772238e5382fabb72a363b5f41f9fbba
phchris95 <p.henrique95@hotmail.com>
2025-02-04 17:17:46 -0300

    Meu primeiro Commit do projeto catao de visita do mario

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueinobotao () {
+
+    form.style.left = "40%"
+    form.style.transform = "translatex{-50%}"
+    mascara.style.visibility = "visible"
+
+}
+
+function sumirform () {
+    
+    form.style.left = "-17.6%"
+    mascara.style.visibility = "hidden"
+
+}
