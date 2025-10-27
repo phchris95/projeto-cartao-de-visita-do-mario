7382ece9e417df68f4df7318a0878b51325ad996
phchris95 <p.henrique95@hotmail.com>
2025-10-27 17:24:59 -0300

    Atualizei o arquivo .js para que o menu hamburguer funcione corretamente (função abrir e fechar o menu)

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
+const menu = document.querySelector('.menu-links');
+const mascaraMenu = document.querySelector(".mascara-menu")
+const btn = document.querySelector(".menu-toggle")
 
 function cliqueinobotao () {
 
@@ -9,9 +12,52 @@ function cliqueinobotao () {
 
 }
 
-function sumirform () {
+function sumirForm () {
     
-    form.style.left = "-149.5px"
+    form.style.left = "-150px"
     mascara.style.visibility = "hidden"
 
 }
+
+function toggleMenu() {
+    
+    menu.classList.add('show');
+    mascaraMenu.style.visibility = "visible"
+}
+
+function sumirMenu () {
+    
+    mascaraMenu.style.visibility = "hidden"
+    menu.classList.remove('show');
+}
+
+document.addEventListener('click', function(e) {
+
+    if( !menu.contains(e.target) && !btn.contains(e.target) ){
+
+        sumirMenu();
+
+    } else {
+
+
+
+    }
+
+})
+
+btn.addEventListener('click', () => {
+
+    const isOpen = menu.classList.contains('show');
+    if (isOpen) {
+
+        //fecha o menu
+        sumirMenu();
+    } else{
+
+        //abre o menu 
+        menu.classList.add('show');
+        mascaraMenu.style.visibility = "visible";
+
+    }
+
+})
